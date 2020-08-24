c92fb87681c37b89924a270997131a0703a1bf6f
jkaplanis <johntkaplanis@gmail.com>
2020-08-23 20:47:57 -0700

    connect meet the blogger component to contentful

--- src/components/MeetTheBlogger.js
+++ src/components/MeetTheBlogger.js
@@ -27,20 +27,46 @@ const Content = styled.div`
 `
 
 const MeetTheBlogger = props => {
-  console.log(props.theme)
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulBlogger {
+        edges {
+          node {
+            bloggerImage {
+              file {
+                url
+              }
+              title
+            }
+            bloggerTagline {
+              content {
+                content {
+                  value
+                }
+              }
+            }
+            bloggerName
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <HeaderWrapper>
       <ImageWrapper>
         <img
-          src="https://images.squarespace-cdn.com/content/v1/53ed0e3ce4b0c296acaeae80/1584577511464-8FDZYWQVXUI1OBS4VTZP/ke17ZwdGBToddI8pDm48kD8Xroq_AX5Zgi3HGn2a5gd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UQQ0iLpUJJ55dW55w_oZ6JvHnXlmx4oSoNzVwlPskgpsLIXfY3DEqu8fc08UsQJ-4w/Bonneville14082-Edit-DHWEB%2BNick%2BFerguson%2BDenver%2BBroncos%2BHeadshot%2BPhotography%2Bby%2BAaron%2BLucy%2BDenver%2BColorado%2BHeadshots%2BPhotographer.jpg?format=2500w"
-          alt="Amber Kaplanis Headshot"
+          src={data.allContentfulBlogger.edges[0].node.bloggerImage.file.url}
+          alt={data.allContentfulBlogger.edges[0].node.bloggerImage.file.title}
         ></img>
       </ImageWrapper>
       <Content>
-        <Heading>Amber Kaplanis</Heading>
+        <Heading>{data.allContentfulBlogger.edges[0].node.bloggerName}</Heading>
         <p>
-          Banh mi synth street art chambray food truck yuccie tumblr readymade
-          organic roof party letterpress.
+          {
+            data.allContentfulBlogger.edges[0].node.bloggerTagline.content[0]
+              .content[0].value
+          }
         </p>
       </Content>
     </HeaderWrapper>
